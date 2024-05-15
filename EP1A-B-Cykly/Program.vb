f204d8b85a49224f8d742bcf1662fcfc03faddc4
Jiří Hlaváček <jirka@jhlavacek.cz>
2024-05-15 10:20:50 +0200

    Přidejte soubory projektu.

---
+++ EP1A-B-Cykly/Program.vb
@@ -0,0 +1,13 @@
+Imports System
+
+Module Program
+  Sub Main(args As String())
+    Dim cislo As Integer
+
+    Do
+      Console.Write("Zadej èíslo: ")
+      cislo = Console.ReadLine
+      Console.WriteLine($"Zadané èíslo je {cislo}")
+    Loop
+  End Sub
+End Module
